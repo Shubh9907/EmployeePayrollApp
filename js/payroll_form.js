10332524d5d9f2dc282c97f8e551347c079e4902
Shubh9907 <Shubhamv60047@gmail.com>
2021-12-08 22:59:46 +0530

    Added: Code for validating name

---
+++ js/payroll_form.js
@@ -0,0 +1,18 @@
+const text = document.querySelector('#name');
+const textError = document.querySelector('.text-error');
+text.addEventListener('input', function() {
+    let nameRegex = RegExp("^[A-Z][a-z]{2,} [A-Z][a-z]{2,}$");
+    if (nameRegex.test(text.value))
+    textError.textContent = "";
+    else textError.textContent= "Name is Incorrect";
+});
+
+/**
+ * Event listner for salary range
+ */
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input', function(){ 
+    output.textContent = salary.value;
+});
